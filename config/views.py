ee95223df27568fb5de48a5172e33dde462f3598
Sajjad Behzadi <sajjad.behzadi2020@gmail.com>
2021-04-13 01:42:17 +0430

    add template

--- config/views.py
+++ config/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("<h1>Index Page</h1>")
+    return render(request, 'index.html')
